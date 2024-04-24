f0ad54df4a1dcab76f1d69709f90da6727b9ac07
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-04-24 11:56:13 +0500

    FOR|4-dars

---
+++ app.js
@@ -0,0 +1,10 @@
+// FOR 
+// N{1}
+// let k = +prompt("Iltimos, k sonini kiriting:");
+// let n = +prompt("Iltimos, n sonini kiriting:");
+
+// // For tsikli orqali k sonini n marta chiqaramiz
+// for (let i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
